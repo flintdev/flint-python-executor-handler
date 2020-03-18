b866c9e16da0ee95af39c3d551b18b9d9964b2c9
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-18 13:41:40 -0700

    update __init__.py

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,4 +1,10 @@
 from .version import __version__
-import handler.flow_data
-import handler.handler_helper
-import handler.model
+from .handler_helper import Handler
+from .flow_data import FlowDataException
+
+# if somebody does "from handler import *", this is what they will
+# be able to access:
+__all__ = [
+    'Handler',
+    'FlowDataException',
+]
