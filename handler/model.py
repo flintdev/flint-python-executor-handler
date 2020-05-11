2007df77984319fd2a496b5ccb222d1833efc0af
Gaoxin Dai <daigx1990@gmmail.com>
2020-05-11 14:23:32 -0700

    version 0.0.3

--- handler/model.py
+++ handler/model.py
@@ -1,3 +1,15 @@
+from kubernetes import client, config
+import json
+from kubernetes.client.rest import ApiException
+
+config.load_kube_config()
+api = client.CustomObjectsApi()
+
+
 class Model:
     def __init__(self):
-        pass
+        self.obj_name = ""
+        self.group = ""
+        self.version = ""
+        self.namespace = ""
+        self.plural = ""
